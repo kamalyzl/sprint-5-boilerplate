01f404bfb7d34b3dbc81f96328a80f1cb12aee49
kamalyzl <kamaly.kazal@gmail.com>
2018-02-20 12:45:30 -0500

    diseño index terminado, funcionalidad crear un nuevo topic y data de respuestas

--- public/js/main.js
+++ public/js/main.js
@@ -17,16 +17,23 @@ $.ajax({
     autocompleteText(result);
   }
 }).done(function() {
- 
 });
 
 // Muestra todo el contenido
 function showText(result) {
   result.forEach(element => {
-    console.log(element);
+    // console.log(element);
     let $content = $('#content-text');
-    $content.append(` <h2>Por: ${element.author_name}  <span class="totalRespon">${element.responses_count} respuestas</span></h2>
-    <p>Descripción: ${element.content}.</p>`);
+    // $content.append(`<div class="styleContent"> <h2>Tema: ${element.content} <p class="totalRespon" data-id=${element.id}>${element.responses_count} respuestas</p></h2>
+    // <p>Autor: ${element.author_name}.</p></div>`);
+
+    $content.append(`
+                        <tr>
+                          <th scope="row" class="totalRespon bg-dark text-light" data-id=${element.id}>${element.id}</th>
+                          <td  class="totalRespon">${element.content}</td>
+                          <td>${element.author_name}</td>
+                          <td>${element.responses_count}</td>
+                        </tr>`);
   });
 }
 
@@ -37,7 +44,7 @@ function autocompleteText(result) {
   });
 }
 
-
+// Boton que envia la información al API con un POST
 $sendData.click(function() {
   $.post(data,
     {
@@ -45,6 +52,28 @@ $sendData.click(function() {
       content: $('#exampleFormControlTextarea1').val()
     },
     function(data, status) {
-      alert('Data: ' + data + '\nStatus: ' + status);
+      console.log(data);
+      let $content = $('#content-text');
+      let firstChil = $('#content-text').eq(0);
+      // $(firstChil).prepend(` <h2 data-id=${data.id} data-content=${data.content} data-author_name=${data.author_name}>Tema: ${data.content}</h2>
+      //   <p>Autor: ${data.author_name}.</p>`);
+
+      $(firstChil).prepend(`
+        <tr>
+          <th scope="row" class="totalRespon bg-dark text-light" data-id=${data.id}>${data.id}</th>
+          <td class="totalRespon">${data.content}</td>
+          <td>${data.author_name}</td>
+          <td>0</td>
+        </tr>`);
     });
+});
+ 
+ 
+// Cuando el usuario haga click a cualquier etiqueta con la clase  .totalRespon, guardaremos el id
+$(document).on('click', '.totalRespon', function() {
+  console.log($(this));
+  let userId = $(this).data('id');
+  console.log($(this).data('id'));
+  localStorage.setItem('usuario', userId);
+  $(location).attr('href', 'verTopic.html');
 });
--- public/js/verTopic.js
+++ public/js/verTopic.js
@@ -1,6 +1,28 @@
-var topicId = getParameterByName('topic_id');
 
-// Solo por propositos de debug
-if (topicId) {
-  alert('El topic ID es:' + topicId);
+let answer = `http://examen-laboratoria-sprint-5.herokuapp.com/topics/${localStorage.usuario}/responses`;
+getParameterByName('kamaly');
+
+$.ajax({
+  url: answer,
+  data: JSON.stringify(answer),
+  datatype: 'json',
+  context: document.body,
+  contentType: 'application/json',
+  success: function (result) {
+    console.log(result);
+    showText(result);
+    // autocompleteText(result);
+  }
+}).done(function () {
+});
+
+
+// Muestra todo el contenido
+function showText(result) {
+  result.forEach(element => {
+    // console.log(element);
+    let $content = $('#content-text');
+    $content.append(`<h5>Comentario: ${element.content} </h5>
+    <p>Por: ${element.author_name}.</p>`);
+  });
 }
