161ffe4e1c2a27b0259b2b7efff7d0291eb13a8e
kamalyzl <kamaly.kazal@gmail.com>
2018-02-20 00:22:17 -0500

    Agregando post ajax

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ $('#myModal').on('shown.bs.modal', function() {
 });
 
 const $search = $('#search');
+const $sendData = $('#saveData');
 let data = 'https://examen-laboratoria-sprint-5.herokuapp.com/topics';
 
 $.ajax({
@@ -22,6 +23,7 @@ $.ajax({
 // Muestra todo el contenido
 function showText(result) {
   result.forEach(element => {
+    console.log(element);
     let $content = $('#content-text');
     $content.append(` <h2>Por: ${element.author_name}  <span class="totalRespon">${element.responses_count} respuestas</span></h2>
     <p>Descripción: ${element.content}.</p>`);
@@ -35,11 +37,14 @@ function autocompleteText(result) {
   });
 }
 
-// $search.submit(function (event) {
-//   event.preventDefault();
-//   let $content = $('#content-text');
-//   if ($('input:first').val() === element.author_name) {
-//     $content.html('');
-//     alert('son iguales');
-//   }
-// });
+
+$sendData.click(function() {
+  $.post(data,
+    {
+      author_name: $('#formGroupExampleInput').val(),
+      content: $('#exampleFormControlTextarea1').val()
+    },
+    function(data, status) {
+      alert('Data: ' + data + '\nStatus: ' + status);
+    });
+});
